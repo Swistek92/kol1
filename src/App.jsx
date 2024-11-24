85699de32d62e2012436eb8c2f6dd492c278f377
MARMOT <63745881+Swistek92@users.noreply.github.com>
2024-11-24 14:51:17 +0000

    kol1

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,12 @@ import AverageAboveDiagonal from "./zadania/AverageAboveDiagonal";
 export default function App() {
   return (
     <div className="App">
-      {/* <h1>zad1 UniqueNumbers</h1> */}
       <UniqueNumbers />
       <hr />
-      {/* <h2>zad2 MedianCalculator</h2> */}
       <MedianCalculator />
       <hr />
-      {/* <h1>zad3 SymbolNewtona</h1> */}
       <SymbolNewtona />
       <hr />
-{/* ss */}
       <SelectionSort />
       <hr />
 
